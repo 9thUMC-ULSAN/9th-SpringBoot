ea2d74060833c7eb0b7fa4047169ba39e8f56ea6
todotoday <s260822@naver.com>
2025-11-18 21:09:11 +0900

    6주차 포카리

--- 김성원/week4/umc9th2/src/main/java/com/example/umc9th2/domain/review/repository/ReviewRepository.java
+++ 김성원/week4/umc9th2/src/main/java/com/example/umc9th2/domain/review/repository/ReviewRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+
+public interface ReviewRepository extends JpaRepository<Review, Long> , ReviewRepositoryCustom{
 
     // 특정 가게에 등록된 리뷰 목록 조회
     @Query("SELECT r FROM Review r WHERE r.store.id = :storeId ORDER BY r.createdAt DESC")
