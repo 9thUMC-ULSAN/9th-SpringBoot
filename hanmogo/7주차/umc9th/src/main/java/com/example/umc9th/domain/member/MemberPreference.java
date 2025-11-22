47325321c788a4cf0c62b0dc9b4f641b1cc45893
Your Name <you@example.com>
2025-11-23 04:36:40 +0900

    [feat] : 7주차 과제

---
+++ hanmogo/7주차/umc9th/src/main/java/com/example/umc9th/domain/member/MemberPreference.java
@@ -0,0 +1,30 @@
+package com.example.umc9th.domain.member;
+
+import com.example.umc9th.domain.foodCategory.FoodCategory;
+import com.example.umc9th.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "user_preference")
+public class MemberPreference extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_preference_id")
+    private Long id;
+
+    // --- 연관관계 매핑 ---
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_category_id")
+    private FoodCategory foodCategory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+}
